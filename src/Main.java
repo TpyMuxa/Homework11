f0bde3e73b1f6f204410958ae06d6d176f3ccd38
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-21 23:03:54 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        printJobSeparator(1);
+        taskLeapYear(2023);
+    }
 
+    public static void printJobSeparator(int taskNumber) {
+        System.out.println("Задание " + taskNumber);
+    }
+
+    public static void taskLeapYear(int year) {
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
