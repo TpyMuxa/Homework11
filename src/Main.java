251975af6e6c5413ecf2e424c52ac6bed3e147a4
TpyMuxa <tpymuxa1133@gmail.com>
2023-09-21 23:24:47 +0500

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         printJobSeparator(1);
         taskLeapYear(2023);
+        printJobSeparator(2);
+        taskInstall(1, 2023);
     }
 
     public static void printJobSeparator(int taskNumber) {
@@ -13,6 +17,29 @@ public class Main {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void taskInstall(int typeOS, int clientDeviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        boolean oldVersion = clientDeviceYear < currentYear;
+        switch (typeOS) {
+            case 0:
+                if (oldVersion) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1:
+                if (oldVersion) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Приложение банка работает только на iOS или Android");
         }
     }
 }
